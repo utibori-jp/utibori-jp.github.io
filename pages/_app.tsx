27b1ee49b16a2b26ade9030ae91eeb43ce7b833a
utibori-jp <kouki.utibori@gmail.com>
2026-06-02 22:51:17 +0900

    feat: replace Next.js boilerplate with static terminal-style profile page

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// pages/_app.tsx
-import 'tailwindcss/tailwind.css'; // Tailwind CSSのインポート
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
